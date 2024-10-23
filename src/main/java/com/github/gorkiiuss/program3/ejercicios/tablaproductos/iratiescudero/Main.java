373f857d317a47aeef6e8abf2298c61a70b54a7e
iratiescudero <irati.escudero@opendeusto.es>
2024-10-23 11:20:09 +0200

    a

---
+++ src/main/java/com/github/gorkiiuss/program3/ejercicios/tablaproductos/iratiescudero/Main.java
@@ -0,0 +1,9 @@
+package com.github.gorkiiuss.program3.ejercicios.tablaproductos.iratiescudero;
+
+public class Main {
+    public static void main(String[] args) {
+        // Inicializar la ventana principal
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
