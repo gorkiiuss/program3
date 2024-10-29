fc20ed244e7cb0a37d7cfbab2e72ebf5d6355af5
puente.javier <puente.javier@SS23-5008767600>
2024-10-29 09:59:32 +0100

    ej tabla_prod

---
+++ src/main/java/com/github/gorkiiuss/program3/ejercicios/tablaproductos/javierpuente/Main.java
@@ -0,0 +1,9 @@
+package com.github.gorkiiuss.program3.ejercicios.tablaproductos.javierpuente;
+
+public class Main {
+    public static void main(String[] args) {
+        // Inicializar la ventana principal
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
